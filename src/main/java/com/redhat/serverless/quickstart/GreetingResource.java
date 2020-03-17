3b7463926dc3c36eb63896cde553ef21f110ffd5
danieloh30 <doh@redhat.com>
2020-03-17 13:29:10 -0400

    update contents

--- src/main/java/com/redhat/serverless/quickstart/GreetingResource.java
+++ src/main/java/com/redhat/serverless/quickstart/GreetingResource.java
@@ -5,12 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/api/hello")
+@Path("/api/quarkus")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return "Welcome, Quarkus with Serverless!";
     }
 }
